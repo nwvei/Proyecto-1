449b3a23b650f0f17aec3fc512eb482c9a780ff8
nwvei <aaronvillegasl04@gmail.com>
2026-07-24 11:37:25 -0600

    Cambio en tablero
    Uso de metodos de nivel

--- src/Tablero/Tablero.java
+++ src/Tablero/Tablero.java
@@ -17,8 +17,6 @@ public class Tablero {
     private int columnas;
     private Nivel nivelActual;
  
-    private static final int columnasFijas = 4;
- 
     public Tablero(Nivel nivel) {
         inicializarTablero(nivel);
     }
@@ -26,9 +24,9 @@ public class Tablero {
     public void inicializarTablero(Nivel nivel) {
         this.nivelActual = nivel;
  
-        int totalCartas = nivel.getCantidadParejas() * 2;
-        this.columnas = columnasFijas;
-        this.filas = totalCartas / columnasFijas;
+        // Ahora se obtienen directamente del nivel (ej. Principiante: 4x4, Intermedio: 4x8, Avanzado: 8x8)
+        this.filas = nivel.getFilas();
+        this.columnas = nivel.getColumnas();
  
         this.cartas = new Carta[filas][columnas];
         distribuirParejas(nivel.getCantidadParejas());
@@ -36,23 +34,22 @@ public class Tablero {
  
     private static final String[] ImagenesR = {
         "/imagenes/carta1.png", "/imagenes/carta2.png", "/imagenes/carta3.png", "/imagenes/carta4.png",
-"/imagenes/carta5.png", "/imagenes/carta6.png", "/imagenes/carta7.png", "/imagenes/carta8.png",
-"/imagenes/carta9.png", "/imagenes/carta10.png", "/imagenes/carta11.png", "/imagenes/carta12.png",
-"/imagenes/carta13.png", "/imagenes/carta14.png", "/imagenes/carta15.png", "/imagenes/carta16.png",
-"/imagenes/carta17.png", "/imagenes/carta18.png", "/imagenes/carta19.png", "/imagenes/carta20.png",
-"/imagenes/carta21.png", "/imagenes/carta22.png", "/imagenes/carta23.png", "/imagenes/carta24.png",
-"/imagenes/carta25.png", "/imagenes/carta26.png", "/imagenes/carta27.png", "/imagenes/carta28.png",
-"/imagenes/carta29.png", "/imagenes/carta30.png", "/imagenes/carta31.png", "/imagenes/carta32.png",
-"/imagenes/carta33.png", "/imagenes/carta34.png", "/imagenes/carta35.png", "/imagenes/carta36.png",
-"/imagenes/carta37.png", "/imagenes/carta38.png", "/imagenes/carta39.png", "/imagenes/carta40.png",
-"/imagenes/carta41.png", "/imagenes/carta42.png", "/imagenes/carta43.png", "/imagenes/carta44.png",
-"/imagenes/carta45.png", "/imagenes/carta46.png", "/imagenes/carta47.png", "/imagenes/carta48.png",
-"/imagenes/carta49.png", "/imagenes/carta50.png", "/imagenes/carta51.png", "/imagenes/carta52.png",
-"/imagenes/carta53.png", "/imagenes/carta54.png", "/imagenes/carta55.png", "/imagenes/carta56.png",
-"/imagenes/carta57.png", "/imagenes/carta58.png", "/imagenes/carta59.png", "/imagenes/carta60.png",
-"/imagenes/carta61.png", "/imagenes/carta62.png", "/imagenes/carta63.png", "/imagenes/carta64.png"
+        "/imagenes/carta5.png", "/imagenes/carta6.png", "/imagenes/carta7.png", "/imagenes/carta8.png",
+        "/imagenes/carta9.png", "/imagenes/carta10.png", "/imagenes/carta11.png", "/imagenes/carta12.png",
+        "/imagenes/carta13.png", "/imagenes/carta14.png", "/imagenes/carta15.png", "/imagenes/carta16.png",
+        "/imagenes/carta17.png", "/imagenes/carta18.png", "/imagenes/carta19.png", "/imagenes/carta20.png",
+        "/imagenes/carta21.png", "/imagenes/carta22.png", "/imagenes/carta23.png", "/imagenes/carta24.png",
+        "/imagenes/carta25.png", "/imagenes/carta26.png", "/imagenes/carta27.png", "/imagenes/carta28.png",
+        "/imagenes/carta29.png", "/imagenes/carta30.png", "/imagenes/carta31.png", "/imagenes/carta32.png",
+        "/imagenes/carta33.png", "/imagenes/carta34.png", "/imagenes/carta35.png", "/imagenes/carta36.png",
+        "/imagenes/carta37.png", "/imagenes/carta38.png", "/imagenes/carta39.png", "/imagenes/carta40.png",
+        "/imagenes/carta41.png", "/imagenes/carta42.png", "/imagenes/carta43.png", "/imagenes/carta44.png",
+        "/imagenes/carta45.png", "/imagenes/carta46.png", "/imagenes/carta47.png", "/imagenes/carta48.png",
+        "/imagenes/carta49.png", "/imagenes/carta50.png", "/imagenes/carta51.png", "/imagenes/carta52.png",
+        "/imagenes/carta53.png", "/imagenes/carta54.png", "/imagenes/carta55.png", "/imagenes/carta56.png",
+        "/imagenes/carta57.png", "/imagenes/carta58.png", "/imagenes/carta59.png", "/imagenes/carta60.png",
+        "/imagenes/carta61.png", "/imagenes/carta62.png", "/imagenes/carta63.png", "/imagenes/carta64.png"
     };
- 
  
     private void distribuirParejas(int cantidadParejas) {
         int totalCartas = cantidadParejas * 2;
@@ -67,6 +64,7 @@ public class Tablero {
             contador++;
         }
  
+        // Barajar las imágenes aleatoriamente
         for (int i = totalCartas - 1; i > 0; i--) {
             int j = (int) (Math.random() * (i + 1));
             String temporal = imagenes[i];
@@ -114,4 +112,3 @@ public class Tablero {
         return columnas;
     }
 }
- 
